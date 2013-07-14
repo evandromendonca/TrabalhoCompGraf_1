1b8b10a1cebde97fc7879e8a7c81691b2b948e28
evandromendonca <evandro.user@gmail.com>
2013-07-14 16:15:01 -0300

    criando o repo

---
+++ TrabalhoCompGraf_1/TrabalhoCompGraf_1/TrabalhoCompGraf_1.cpp
@@ -0,0 +1,12 @@
+// TrabalhoCompGraf_1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "gl/glut.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
